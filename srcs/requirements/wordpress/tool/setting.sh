43f7c0b58ed10aecd76d7b871a807931f40dce0c
bayamasa <mapi0213mgt@gmail.com>
2023-03-08 10:18:58 +0900

    wordpressの設定を更新

--- srcs/requirements/wordpress/tool/setting.sh
+++ srcs/requirements/wordpress/tool/setting.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 
+# データベースにアクセスできるまで確認するようにしないと落ちるかも
 while ! mysqladmin ping -h mariadb -u wordpress -pwordpress > /dev/null 2>&1
 do
-    sleep 1
+    sleep 3
 done 
 
-wp core download --path=/var/www/html/wordpress --allow-root
-wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=mariadb:3306 --allow-root --skip-check --path=/var/www/html/wordpress
-wp core install --url=http://example.com/ --title=test --admin_user=wordpress_user --admin_password=password --admin_email=test@example.com --allow-root --path=/var/www/html/wordpress
+if ! wp core is-installed --allow-root --path=/var/www/html &> /dev/null ; then
 
-mkdir -p /run/php
+    echo "Installing wordpress..."
+    wp core download --path=/var/www/html --allow-root
+    wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=mariadb --allow-root --skip-check --path=/var/www/html --extra-php \
+    
+
+    wp core install --url=localhost:8080 --title=localhost --admin_user=wordpress_user --admin_password=password --admin_email=hello@example.com --allow-root --path=/var/www/html
+
+    wp user create --allow-root wordpress hello@gmail.com --user_pass=wordpress --role=author --path=/var/www/html
+
+fi
+
+chmod -R 775 /var/www/*
+chown -R www-data:www-data /var/www/*
+mkdir -p /run/php/
 
 exec "$@"
