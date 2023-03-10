1aecd464fa47560aba4b341dbc137ffb41154a97
bayamasa <mapi0213mgt@gmail.com>
2023-03-10 17:12:44 +0900

    コメント追加

--- srcs/requirements/nginx/tools/setting.sh
+++ srcs/requirements/nginx/tools/setting.sh
@@ -2,8 +2,11 @@
 
 mkdir -p /etc/nginx/ssl
 
+# 秘密鍵作成
 openssl genrsa -out /etc/nginx/ssl/server.key 2048
+# 情報付加した公開鍵作成
 openssl req -new -key /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.csr -subj "/C=JP/ST=Tokyo/L=Roppongi/O=42Tokyo/OU=Student/CN=mhirabay"
+# サーバー証明書作成
 openssl x509 -days 3650 -req -signkey /etc/nginx/ssl/server.key -in /etc/nginx/ssl/server.csr -out /etc/nginx/ssl/server.crt
 
 
