0f070e1ef54cc93df5a1640bafa6a9d9b94da10e
bayamasa <mapi0213mgt@gmail.com>
2023-03-11 23:07:10 +0900

    common name修正

--- srcs/requirements/nginx/tools/setting.sh
+++ srcs/requirements/nginx/tools/setting.sh
@@ -5,7 +5,7 @@ mkdir -p /etc/nginx/ssl
 # 秘密鍵作成
 openssl genrsa -out /etc/nginx/ssl/server.key 2048
 # 情報付加した公開鍵作成
-openssl req -new -key /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.csr -subj "/C=JP/ST=Tokyo/L=Roppongi/O=42Tokyo/OU=Student/CN=mhirabay"
+openssl req -new -key /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.csr -subj "/C=JP/ST=Tokyo/L=Roppongi/O=42Tokyo/OU=Student/CN=mhirabay.42.fr"
 # サーバー証明書作成
 openssl x509 -days 3650 -req -signkey /etc/nginx/ssl/server.key -in /etc/nginx/ssl/server.csr -out /etc/nginx/ssl/server.crt
 
